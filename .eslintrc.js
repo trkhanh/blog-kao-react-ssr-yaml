c716eeb106f36fe3f1d683424dce4489614d128e
Khanh <tran_khanh@outlook.com>
2021-01-19 09:41:25 +0700

    init

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react/recommended",
+      "prettier/@typescript-eslint",
+    ],
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  };
