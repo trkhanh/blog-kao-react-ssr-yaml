c181164367c84c29e7fc0e549314aa14246e38c9
Khanh <tran_khanh@outlook.com>
2021-02-23 23:35:29 +0700

    implement unitest for tags

---
+++ tests/components/tags.test.tsx
@@ -0,0 +1,46 @@
+
+import * as React from "react";
+import { render, unmountComponentAtNode } from 'react-dom'
+import { act } from "react-dom/test-utils";
+import { MemoryRouter } from "react-router-dom";
+import Tags from "src/shared/components/Tags";
+
+
+let container: HTMLDivElement = null;
+
+beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container)
+})
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null
+})
+
+describe("<Tags/>", () => {
+    it("renders emply span for a post with no tags", () => {
+        //Edge case 
+        act(() => {
+            render(
+                <MemoryRouter>
+                    <Tags tags={[]} />
+                </MemoryRouter>, container
+            )
+        });
+        expect(container.querySelectorAll("a")).toHaveLength(0)
+    });
+
+    it("renders a single tag", () => {
+        const tags = ["Algorithms"];
+        act(() => {
+            render(
+                <MemoryRouter>
+                    <Tags tags={tags} />
+                </MemoryRouter>
+                , container)
+        });
+        expect(container.querySelectorAll("a")).toHaveLength(1);
+    })
+})
--- tests/xml.test.ts
+++ tests/xml.test.ts
@@ -10,4 +10,41 @@ describe("Leaf.toString", () => {
     const leaf = node("leaf", "content", attributes);
     expect(leaf.toString()).toMatch(/^<leaf key1="value1" key2="value2"/);
   });
+
+  it("serializes content", () => {
+    const leaf = node('leaf', "content");
+    expect(leaf.toString()).toEqual("<leaf>content</leaf>");
+  })
+
+  it("serializes self-closing tags", () => {
+    let leaf = node('leaf');
+    expect(leaf.toString()).toEqual("<leaf />");
+
+    leaf = node("leaf", "", attributes)
+    expect(leaf.toString()).toEqual('<leaf key1="value1" key2="value2" />')
+  })
 });
+
+
+describe("Branch.toString", () => {
+  it("serializes attributes", () => {
+    const leaf = node("leaf", "content");
+    const root = node("root", [leaf], attributes);
+    expect(root.toString()).toMatch(/^<root key1="value1" key2="value2">/);
+  })
+
+  it("serializes nested elements", () => {
+    const leaf = node("leaf", "content")
+    const root = node("root", [leaf]);
+    expect(root.toString()).toEqual("<root><leaf>content</leaf></root>")
+  })
+
+  it("serializes siblings", () => {
+    const author = node("author", "John Doe");
+    const item1 = node("item", "Hello")
+    const item2 = node("item", "World");
+    const root = node("root", [author, item1, item2]);
+    expect(root.toString()).toEqual("<root><author>John Doe</author><item>Hello</item><item>World</item></root>")
+
+  })
+})
