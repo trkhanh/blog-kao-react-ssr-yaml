6da03f241059d2a724f88d00b941381722aa3292
Khanh <tran_khanh@outlook.com>
2021-02-24 09:06:27 +0700

    bdd test for Date component

--- tests/components/date.test.tsx
+++ tests/components/date.test.tsx
@@ -1,56 +1,56 @@
-import * as React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
+import * as React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 
 import Date from "shared/components/Date";
 
-describe('<Date />', () => {
-    let container: HTMLDivElement = null;
+describe("<Date />", () => {
+  let container: HTMLDivElement = null;
 
-    beforeEach(() => {
-        container = document.createElement('div')
-        document.body.appendChild(container)
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+
+  it("renders UNIX birthday", () => {
+    act(() => {
+      render(<Date timestamp={0} />, container);
     });
 
-    afterEach(() => {
-        unmountComponentAtNode(container);
-        container.remove();
-        container = null;
+    expect(container.textContent).toBe("1 January 1970");
+  });
+
+  it("renders current time", () => {
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const now = new global.Date();
+
+    const day = now.getDate();
+    const month = monthNames[now.getMonth()];
+    const year = now.getFullYear();
+
+    act(() => {
+      render(<Date timestamp={now.getTime()} />, container);
     });
-
-    it('renders UNIX birthday', () => {
-        act(() => {
-            render(<Date timestamp={0} />, container);
-        });
-
-        expect(container.textContent).toBe("1 January 1970");
-    })
-
-    it('renders current time', () => {
-        const monthNames = [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December",
-        ];
-
-        const now = new global.Date();
-
-        const day = now.getDate();
-        const month = monthNames[now.getMonth()];
-        const year = now.getFullYear();
-
-        act(() => {
-            render(<Date timestamp={now.getTime()} />, container);
-        });
-        expect(container.textContent).toBe(`${day} ${month} ${year}`);
-    });
+    expect(container.textContent).toBe(`${day} ${month} ${year}`);
+  });
 });
