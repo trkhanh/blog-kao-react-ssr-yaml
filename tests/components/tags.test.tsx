bbf822df06915134db0b7d3ef71d82ba6167e3ae
Khanh <tran_khanh@outlook.com>
2021-02-24 08:57:42 +0700

    update

--- tests/components/tags.test.tsx
+++ tests/components/tags.test.tsx
@@ -7,71 +7,71 @@ import Tags from "src/shared/components/Tags";
 let container: HTMLDivElement = null;
 
 beforeEach(() => {
-    container = document.createElement("div");
-    document.body.appendChild(container);
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
 
 afterEach(() => {
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
 });
 
 describe("<Tags/>", () => {
-    it("renders emply span for a post with no tags", () => {
-        //Edge case
-        act(() => {
-            render(
-                <MemoryRouter>
-                    <Tags tags={[]} />
-                </MemoryRouter>,
-                container
-            );
-        });
-        expect(container.querySelectorAll("a")).toHaveLength(0);
+  it("renders emply span for a post with no tags", () => {
+    //Edge case
+    act(() => {
+      render(
+        <MemoryRouter>
+          <Tags tags={[]} />
+        </MemoryRouter>,
+        container
+      );
     });
+    expect(container.querySelectorAll("a")).toHaveLength(0);
+  });
 
-    it("renders a single tag", () => {
-        const tags = ["Algorithms"];
-        act(() => {
-            render(
-                <MemoryRouter>
-                    <Tags tags={tags} />
-                </MemoryRouter>,
-                container
-            );
-        });
-        expect(container.querySelectorAll("a")).toHaveLength(1);
+  it("renders a single tag", () => {
+    const tags = ["Algorithms"];
+    act(() => {
+      render(
+        <MemoryRouter>
+          <Tags tags={tags} />
+        </MemoryRouter>,
+        container
+      );
     });
+    expect(container.querySelectorAll("a")).toHaveLength(1);
+  });
 
-    it("renders multiple tags", () => {
-        const tags = ["Algorithms", "Python"];
-        act(() => {
-            render(
-                <MemoryRouter>
-                    <Tags tags={tags} />
-                </MemoryRouter>,
-                container
-            );
-        });
-        expect(container.querySelectorAll("a")).toHaveLength(tags.length);
+  it("renders multiple tags", () => {
+    const tags = ["Algorithms", "Python"];
+    act(() => {
+      render(
+        <MemoryRouter>
+          <Tags tags={tags} />
+        </MemoryRouter>,
+        container
+      );
     });
+    expect(container.querySelectorAll("a")).toHaveLength(tags.length);
+  });
 });
 
 describe("<Tag/>", () => {
-    it("navigates to correct route when clicked", () => {
-        const tags = ["Algorithms", "Python"];
+  it("navigates to correct route when clicked", () => {
+    const tags = ["Algorithms", "Python"];
 
-        act(() => {
-            render(
-                <MemoryRouter>
-                    <Tags tags={tags} />
-                </MemoryRouter>,
-                container
-            );
-        });
-
-        const firstLink = container.querySelector("a").href;
-        expect(firstLink.endsWith(`/post?tag=${tags[0]}`));
+    act(() => {
+      render(
+        <MemoryRouter>
+          <Tags tags={tags} />
+        </MemoryRouter>,
+        container
+      );
     });
+
+    const firstLink = container.querySelector("a").href;
+    expect(firstLink.endsWith(`/post?tag=${tags[0]}`));
+  });
 });
