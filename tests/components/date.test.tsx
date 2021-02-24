2f75a593db00044d7102a4ea9df7d78a953ac01c
Khanh <tran_khanh@outlook.com>
2021-02-24 09:05:58 +0700

    bdd test for component Date

---
+++ tests/components/date.test.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from "react-dom/test-utils";
+
+import Date from "shared/components/Date";
+
+describe('<Date />', () => {
+    let container: HTMLDivElement = null;
+
+    beforeEach(() => {
+        container = document.createElement('div')
+        document.body.appendChild(container)
+    });
+
+    afterEach(() => {
+        unmountComponentAtNode(container);
+        container.remove();
+        container = null;
+    });
+
+    it('renders UNIX birthday', () => {
+        act(() => {
+            render(<Date timestamp={0} />, container);
+        });
+
+        expect(container.textContent).toBe("1 January 1970");
+    })
+
+    it('renders current time', () => {
+        const monthNames = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        ];
+
+        const now = new global.Date();
+
+        const day = now.getDate();
+        const month = monthNames[now.getMonth()];
+        const year = now.getFullYear();
+
+        act(() => {
+            render(<Date timestamp={now.getTime()} />, container);
+        });
+        expect(container.textContent).toBe(`${day} ${month} ${year}`);
+    });
+});
