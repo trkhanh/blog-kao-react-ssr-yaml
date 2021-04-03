fc20c238a343dc6d153cbd4edac8b62c55e5185c
Khanh <tran_khanh@outlook.com>
2021-04-03 11:42:40 +0700

    update all images resources

---
+++ src/shared/images/index.tsx
@@ -0,0 +1,5 @@
+export { default as Avatar } from "./avatar.jpg";
+export { default as Favicon } from "./favicon.png";
+export { default as GitHub } from "./github.png";
+export { default as Loading } from "./loading.svg";
+export { default as RSS } from "./rss.png";
